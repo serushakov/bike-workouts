cf1d10ea3c71d487bba315c2258ee79bc4cf7760
Sergey Ushakov <sergey@ushakov.io>
2021-09-22 12:58:37 +0300

    set up theme

--- app/src/main/java/io/ushakov/bike_workouts/MainActivity.kt
+++ app/src/main/java/io/ushakov/bike_workouts/MainActivity.kt
@@ -1,11 +1,59 @@
 package io.ushakov.bike_workouts
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.ushakov.myapplication.ui.theme.BikeWorkoutsTheme
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            BikeWorkoutsTheme {
+                View()
+            }
+        }
+    }
+
+    @Composable
+    fun AppBar() {
+        TopAppBar(
+            title = { Text("Hello world") }
+        )
+    }
+
+
+    @Composable
+    fun View() {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Scaffold(
+                topBar = { AppBar() },
+                floatingActionButton = {
+                    FloatingActionButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Add, "Start workout")
+                    }
+                },
+            ) { }
+        }
+    }
+
+    @Preview(widthDp = 480, heightDp = 840)
+    @Composable
+    fun Preview() {
+        BikeWorkoutsTheme {
+            View()
+        }
     }
 }
---
+++ app/src/main/java/io/ushakov/bike_workouts/ui/theme/Color.kt
@@ -0,0 +1,25 @@
+package io.ushakov.bike_workouts.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue900 = Color(0xFF0D47A1)
+val Blue800 = Color(0xFF1565C0)
+val Blue700 = Color(0xFF1976D2)
+val Blue600 = Color(0xFF1E88E5)
+val Blue500 = Color(0xFF2196F3)
+val Blue400 = Color(0xFF42A5F5)
+val Blue300 = Color(0xFF64B5F6)
+val Blue200 = Color(0xFF90CAF9)
+val Blue100 = Color(0xFFBBDEFB)
+val Blue50 = Color(0xFFE0F7FA)
+
+val Orange900 = Color(0xFFFF6F00)
+val Orange800 = Color(0xFFFF8F00)
+val Orange700 = Color(0xFFFFA000)
+val Orange600 = Color(0xFFFFB300)
+val Orange500 = Color(0xFFFFC107)
+val Orange400 = Color(0xFFFFCA28)
+val Orange300 = Color(0xFFFFD54F)
+val Orange200 = Color(0xFFFFE082)
+val Orange100 = Color(0xFFFFECB3)
+val Orange50 = Color(0xFFFFF8E1)
