30ed85cc9b034c1f33058162ede5732c8d3c3714
Sergey Ushakov <sergey@ushakov.io>
2021-09-23 20:26:13 +0300

    added a bottom app bar with fab and user icon

--- app/src/main/java/io/ushakov/bike_workouts/MainActivity.kt
+++ app/src/main/java/io/ushakov/bike_workouts/MainActivity.kt
@@ -2,13 +2,16 @@ package io.ushakov.bike_workouts
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.*
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import io.ushakov.myapplication.ui.theme.BikeWorkoutsTheme
 
@@ -26,9 +29,13 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun AppBar() {
-        TopAppBar(
-            title = { Text("Hello world") }
-        )
+        BottomAppBar(
+            cutoutShape = RoundedCornerShape(50)
+        ) {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Filled.AccountCircle, stringResource(R.string.account_button_label))
+            }
+        }
     }
 
 
@@ -39,13 +46,16 @@ class MainActivity : ComponentActivity() {
                 .fillMaxSize()
         ) {
             Scaffold(
-                topBar = { AppBar() },
+                bottomBar = { AppBar() },
                 floatingActionButton = {
                     FloatingActionButton(onClick = { /*TODO*/ }) {
-                        Icon(Icons.Filled.Add, "Start workout")
+                        Icon(Icons.Filled.Add, stringResource(R.string.start_workout_label))
                     }
                 },
-            ) { }
+                isFloatingActionButtonDocked = true,
+                floatingActionButtonPosition = FabPosition.Center,
+
+                ) { }
         }
     }
 
