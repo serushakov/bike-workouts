ddfad5e9ee709ee364fd7d613f18895c5090c7fa
Sergey Ushakov <sergey@ushakov.io>
2021-10-06 16:04:09 +0300

    added row of info

--- app/src/main/java/io/ushakov/bike_workouts/ui/views/InWorkout.kt
+++ app/src/main/java/io/ushakov/bike_workouts/ui/views/InWorkout.kt
@@ -3,18 +3,20 @@ package io.ushakov.bike_workouts.ui.views
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextGeometricTransform
 import androidx.compose.ui.unit.dp
 import io.ushakov.bike_workouts.db.entity.WorkoutComplete
 import io.ushakov.bike_workouts.ui.components.ComposableMap
+import io.ushakov.bike_workouts.ui.theme.Typography
 
 @Composable
 fun InWorkout(workoutComplete: WorkoutComplete, onWorkoutStopClick: () -> Unit) {
@@ -43,14 +45,43 @@ fun InWorkout(workoutComplete: WorkoutComplete, onWorkoutStopClick: () -> Unit) 
                 .padding(16.dp)
                 .fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally) {
+                InfoRow(
+                    Row(text = "25.0", title = "kmh"),
+                    Row(text = "120", title = "bpm"),
+                    Row(text = "0.10.34", title = "time"),
+                )
                 PauseButton {
                     showButton = !showButton
                 }
-                if(showButton) {
+                if (showButton) {
                     PauseButton {
                         showButton = !showButton
                     }
                 }
+            }
+        }
+    }
+}
+
+class Row(
+    val title: String,
+    val text: String,
+)
+
+@Composable
+fun InfoRow(vararg rows: Row) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        rows.map {
+            Column(Modifier.weight(1f), horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(it.text, style = Typography.h4)
+                Spacer(Modifier.height(4.dp))
+                Text(it.title.uppercase(),
+                    style = Typography.overline,
+                    color = MaterialTheme.colors.primary,
+                    fontWeight = FontWeight.SemiBold)
             }
         }
     }
@@ -65,3 +96,4 @@ fun PauseButton(onClick: () -> Unit) {
         Icon(Icons.Default.Pause, "Pause", modifier = Modifier.size(42.dp))
     }
 }
+
