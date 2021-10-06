4e9f34e4b6b2df3c74ed92fd46608b4884bbaef6
Sergey Ushakov <sergey@ushakov.io>
2021-10-06 15:32:13 +0300

    set up content animations

--- app/src/main/java/io/ushakov/bike_workouts/ui/views/InWorkout.kt
+++ app/src/main/java/io/ushakov/bike_workouts/ui/views/InWorkout.kt
@@ -1,15 +1,20 @@
 package io.ushakov.bike_workouts.ui.views
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import io.ushakov.bike_workouts.db.entity.WorkoutComplete
+import io.ushakov.bike_workouts.ui.components.ComposableMap
 
 @Composable
 fun InWorkout(workoutComplete: WorkoutComplete, onWorkoutStopClick: () -> Unit) {
@@ -21,13 +26,42 @@ fun InWorkout(workoutComplete: WorkoutComplete, onWorkoutStopClick: () -> Unit) 
 
     if (workout == null || heartRates == null || locations == null) return
 
-    Surface {
-        Column(Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = onWorkoutStopClick) {
-                Text("STOP")
+    var showButton by remember { mutableStateOf(false) }
+
+    Column(Modifier.fillMaxSize()) {
+        ComposableMap(onInit = {}, modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f))
+        Surface(
+            Modifier
+                .offset(y = (-16).dp)
+                .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+                .fillMaxWidth()
+                .animateContentSize()
+        ) {
+            Column(Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                PauseButton {
+                    showButton = !showButton
+                }
+                if(showButton) {
+                    PauseButton {
+                        showButton = !showButton
+                    }
+                }
             }
         }
+    }
+}
+
+@Composable
+fun PauseButton(onClick: () -> Unit) {
+    FloatingActionButton(
+        onClick,
+        Modifier.size(80.dp)
+    ) {
+        Icon(Icons.Default.Pause, "Pause", modifier = Modifier.size(42.dp))
     }
 }
