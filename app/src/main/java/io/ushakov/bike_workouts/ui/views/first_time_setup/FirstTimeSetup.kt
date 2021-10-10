0dc9d8fca54b064e949791608759499678eabde8
Sergey Ushakov <sergey@ushakov.io>
2021-10-10 14:30:06 +0300

    created name submit view

--- app/src/main/java/io/ushakov/bike_workouts/ui/views/first_time_setup/FirstTimeSetup.kt
+++ app/src/main/java/io/ushakov/bike_workouts/ui/views/first_time_setup/FirstTimeSetup.kt
@@ -1,46 +1,45 @@
 package io.ushakov.bike_workouts.ui.views.first_time_setup
 
-import android.Manifest
 import android.Manifest.permission.ACCESS_FINE_LOCATION
-import android.content.pm.PackageManager
 import android.util.Log
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.insets.LocalWindowInsets
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionRequired
 import com.google.accompanist.permissions.rememberPermissionState
+import io.ushakov.bike_workouts.R
+import io.ushakov.bike_workouts.ui.theme.Typography
 
 @Composable
 fun FirstTimeSetup() {
     val navController = rememberNavController()
+    var nameInputValue by remember { mutableStateOf<String?>(null) }
 
 
     NavHost(navController = navController, startDestination = "name") {
         composable("name") {
-            Surface(
-                modifier = Modifier.fillMaxHeight()
-            ) {
-                Column {
-
-                    Text(text = "Name")
-                    Button(onClick = { navController.navigate("measurements") }) {
-                        Text("Next")
-                    }
-                }
+            Name(initialName = nameInputValue) {
+                nameInputValue = it
+                navController.navigate("measurements")
             }
         }
         composable("measurements") {
@@ -77,6 +76,68 @@ fun FirstTimeSetup() {
                 modifier = Modifier.fillMaxHeight()
             ) {
                 Text(text = "Finish")
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Name(initialName: String?, onNameSubmit: (name: String) -> Unit) {
+    var nameInputValue by remember { mutableStateOf(initialName ?: "") }
+    val insets = LocalWindowInsets.current
+
+    val imeBottom = with(LocalDensity.current) { insets.ime.bottom.toDp() }
+
+    Log.d("insets", imeBottom.toString())
+
+    Surface {
+        Column(Modifier.fillMaxSize()) {
+            Box(Modifier
+                .weight(1f)
+                .padding(horizontal = 30.dp)
+                .fillMaxWidth()) {
+                Text(
+                    text = stringResource(R.string.intro__name__titile),
+                    style = Typography.h2,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+            Column(Modifier
+                .weight(1f)
+                .padding(horizontal = 30.dp)
+                .padding(bottom = 30.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.intro__name__subtitle),
+                    style = Typography.h5
+                )
+                Spacer(Modifier.height(16.dp))
+                TextField(
+                    value = nameInputValue,
+                    onValueChange = { nameInputValue = it },
+                    label = { Text(stringResource(R.string.intro__name__input_label)) },
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        capitalization = KeyboardCapitalization.Words,
+                        autoCorrect = false,
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = { onNameSubmit(nameInputValue) }
+                    )
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Button(
+                    onClick = { onNameSubmit(nameInputValue) },
+                    enabled = nameInputValue.trim() !== "",
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                ) {
+                    Text(
+                        text = stringResource(R.string.intro__continue_button)
+                    )
+                }
             }
         }
     }
